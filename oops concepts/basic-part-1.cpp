c39ac1483a5f72fa362b0239f980ccb27a75de6b
Kunal Pal <strive007boy@gmail.com>
2025-11-04 11:20:40 +0530

    C-plus oops concepts begin added

---
+++ oops concepts/basic-part-1.cpp
@@ -0,0 +1,41 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// so classes are the userDefine data type 
+// which has the variables and methods togather
+// class is a tamplet/blueprint of an object
+
+class Students{ 
+
+    public:
+    string name;
+    int roll_no;
+
+    public:
+
+    void print(){
+        cout << "Name is " << name << " roll no : " << roll_no << endl;
+    }
+};
+
+
+
+int main(){
+
+    Students s; // this is how an object is created 
+    //object is an instance of class 
+    // object can be a real world entity like person , car , pen , chasma
+    s.name = "Kunal Pal";
+    s.roll_no = 55;
+
+    cout << s.name << endl;
+    cout << s.roll_no << endl;
+
+    s.print();
+
+    cout << sizeof(Students) << endl;
+    return 0;
+
+}
---
+++ oops concepts/basic-part-2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+
+class Shape{
+    
+    private:
+    int sides;
+    string shape_name;
+
+    public:
+
+    Shape(int s , string sName){
+
+        sides = s;
+        shape_name = sName;
+
+    }
+
+    void print(){
+        cout << "sides are "  << sides << endl;
+
+        cout <<"shape are " << shape_name << endl;
+    }
+
+
+
+};
+
+
+int main(){
+    //this is how i can declear the dynamically object which store the data in the heap 
+    // stack has a limited storeage to store the data but 
+    
+    Shape *s1 = new Shape(12 , "Triangle");
+    cout <<sizeof(s1) << endl;
+
+    s1->print();
+
+
+}
